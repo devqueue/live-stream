f1d4a05a480513b9e529179274e5fc6eefd312c0
devqueue <haziq.sayyed@gmail.com>
2021-01-30 19:13:20 +0300

    new code

---
+++ main.py
@@ -0,0 +1,45 @@
+# conditionals
+
+A = 5
+B = 5
+
+if A>B:
+    print("a is greater")
+
+elif A==B:
+    print("a is equal to b") 
+
+else:
+    print("b is greater")
+
+
+# Loops
+
+# for loop 
+
+for i in range(10+1):
+    print(i)
+
+
+# range(start,stop, step)
+
+# While loop
+
+i = 0
+while i<11:
+    print(i)
+    i+=1    
+
+# break and continue
+
+i = 0
+
+while i<10:
+    print(i)
+    i+=1
+    if i==8:
+        break
+
+
+
+
